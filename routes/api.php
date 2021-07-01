5b45d54dfa8ca5ce50e0b57d5966a555b69cafb4
Cooldogs111 <59568739+S1113809@users.noreply.github.com>
2021-07-01 13:56:03 +0200

    Created authentication (login, register, logout)

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/subjects', '\App\Http\Controllers\SubjectController@index');
 Route::get('/goals', '\App\Http\Controllers\GoalController@index');
+
+
+Route::post('/register', '\App\Http\Controllers\AuthController@register');
+Route::post('/login','\App\Http\Controllers\AuthController@login');
+
+Route::group(['middleware'=> ['auth:sanctum']],function(){
+    Route::post('/logout', '\App\Http\Controllers\AuthController@logout');
+});
